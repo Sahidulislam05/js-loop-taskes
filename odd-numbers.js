869194b6af7bdfe2e8c2c5ed94e57af460a86513
Sahidulislam05 <sahidulislamcst@gmail.com>
2025-07-31 13:14:36 +0600

    added numbers section

---
+++ odd-numbers.js
@@ -0,0 +1,39 @@
+/***
+
+Subtask-1:
+
+Display sum of all the odd numbers from 91 to 129.
+
+ */
+/***
+
+Subtask-2:
+
+Display sum of all the even numbers from 51 to 85.
+
+ */
+
+// Subtask 1
+
+let sum = 0;
+
+for (let i = 93; i <= 129; i += 2) {
+  sum = sum + i;
+  console.log(i);
+}
+
+console.log("sum of numbers from 91 to 129 is", sum);
+
+// Subtask 2
+
+/* 
+let sum = 0;
+
+for (let i = 52; i <= 85; i += 2) {
+  sum = sum + i;
+  console.log(i);
+}
+
+console.log("sum of numbers from 51 to 85 is", sum);
+
+*/
