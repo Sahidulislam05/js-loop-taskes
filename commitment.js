b6971d478864e5962efafab019b9d66994813119
Sahidulislam05 <sahidulislamcst@gmail.com>
2025-07-31 12:51:53 +0600

    added commitment problem solution

--- commitment.js
+++ commitment.js
@@ -3,3 +3,15 @@
 "I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
 
  */
+
+let message = "I will invest at least 6 hrs every single day for next 60 days!";
+
+let sum = 0;
+
+for (let i = 1; i <= 60; i++) {
+  sum = sum + 1;
+
+//   console.log(i);
+
+  console.log(message);
+}
